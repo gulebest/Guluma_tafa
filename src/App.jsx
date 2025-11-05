fb44ca94a87c7e93e3b98853796788d4f0cb411b
gulebest <gulumabest694@gmail.com>
2025-11-05 13:00:52 +0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./Components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
